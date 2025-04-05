e798a4c2a73e77ff16bb9265ea81003fbc9a9764
Roberto Barreto <robertoboficial@gmail.com>
2025-04-05 12:54:07 -0300

    Implementando o menu principal com opções para módulos de alunos, professores e disciplinas

--- Projeto_Escola.c
+++ Projeto_Escola.c
@@ -1,8 +1,46 @@
-#include <ctype.h>
-#include <locale.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
+
+
+int main () {
+
+    int opcao; // Opção que o usuário insere no Menu
+    int sair = 0; // Condição de saída
+
+    while (!sair) {
+        // Opções para a visualização do usuário
+        printf("Projeto Escola\n1 - Aluno\n2 - Professor\n3 - Disciplina\n0 - Sair\n");
+        scanf("%d", &opcao); // Leitura do input do usuário
+
+        switch (opcao) {
+            case 0: {
+                sair = 1;
+                break;
+            }
+
+            case 1: {
+                printf("Módulo Aluno\n");
+                break;
+            }
+
+            case 2: {
+                printf("Módulo Professor\n");
+                break;
+            }
+
+            case 3: {
+                printf("Módulo Disciplina\n");
+                break;
+            }
+
+            default: {
+                printf("Opção Inválida\n");
+                break;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
