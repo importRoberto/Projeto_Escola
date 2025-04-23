febc59d28070ebc4bf2b9be89d469f277f890049
Jennifer <129389927+RochaJennifer@users.noreply.github.com>
2025-04-23 11:32:00 -0300

    Add files via upload

---
+++ relatorios.h
@@ -0,0 +1,9 @@
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
+
+#include "funcoes.h"
+
+void ListarAlunos(Aluno alunos[], int qtdAlunos);
+void ExibirMenuRelatorios(Aluno alunos[], int qtdAlunos, Professor professores[], int qtdProfessores, Disciplina disciplinas[], int qtdDisciplinas);
+
+#endif
