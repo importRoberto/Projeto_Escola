880327b515abb0ce5b051943401d3f9040174678
Jennifer <129389927+RochaJennifer@users.noreply.github.com>
2025-04-23 11:31:47 -0300

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,39 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#define MAX 100
+#define MAX_ALUNOS 100
+
+typedef struct {
+    char nome[100];
+    int matricula;
+    char dataNascimento[11];
+    char sexo;
+    char cpf[15];
+} Aluno;
+
+typedef struct {
+    char nome[100];
+    int matricula;
+    char dataNascimento[11];
+    char sexo;
+    char cpf[15];
+} Professor;
+
+typedef struct {
+    char nome[100];
+    char codigo[10];
+    int semestre;
+    char professor[100];
+    int alunosMatriculados[MAX_ALUNOS];
+    int qtdAlunos;
+} Disciplina;
+
+// Protótipos das funções gerais
+void CadastrarAluno(Aluno alunos[], int *qtdAlunos);
+void CadastrarProfessor(Professor professores[], int *qtdProfessores);
+void CadastrarDisciplina(Disciplina disciplinas[], int *qtdDisciplinas);
+void MatricularAlunoEmDisciplina(Disciplina disciplinas[], int qtdDisciplinas, Aluno alunos[], int qtdAlunos);
+void PreencherDadosComuns(char nome[], int *matricula, char dataNascimento[], char *sexo, char cpf[]);
+
+#endif
