2ff79cbf592983d98a99584e2304f04d651048f0
Jennifer <129389927+RochaJennifer@users.noreply.github.com>
2025-04-25 10:50:58 -0300

    Create professores.h
    Declara as funções de professores.

---
+++ professores.h
@@ -0,0 +1,9 @@
+#ifndef PROFESSORES_H
+#define PROFESSORES_H
+
+#include "structs.h"
+
+void CadastrarProfessor(Professor professores[], int *qtdProfessores);
+void removerProfessor(Professor professores[], int *qtdProfessor);
+
+#endif
